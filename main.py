856a723f5fe8c679cc95d3f411831bb6974fe312
Mirzaagha Guliyev <35248449+Mirzaagha@users.noreply.github.com>
2023-12-26 22:29:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
-e = np.ones([2,2])
+e = np.ones([2, 2])
 a = np.array([1])
 for x in range(10):
-    b = np.zeros([3+x,3+x])
-    b[1:2+x,1:2+x] = a
-    d = np.zeros([2+x,2+x])
+    b = np.zeros([3+x, 3+x])
+    b[1:2+x, 1:2+x] = a
+    d = np.zeros([2+x, 2+x])
     s = d.shape
     for r in range(s[0]):
         for c in range(s[1]):
-            d[r,c] = np.sum(b[0+c:2+c,0+r:2+r]*e)
+            d[r, c] = np.sum(b[0+c:2+c, 0+r:2+r]*e)
     print(d)
     print("\n")
     a = d
